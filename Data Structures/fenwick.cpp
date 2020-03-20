4e7b02ac9af9616fba0b665f02e46d83708ca256
hemant2132 <45938556+hemant2132@users.noreply.github.com>
2020-03-21 00:36:38 +0530

    Add files via upload

---
+++ Data Structures/fenwick.cpp
@@ -0,0 +1,30 @@
+int bit[N];
+
+void update(int idx, int val)
+{
+	while(idx<=n)
+	{
+		bit[idx]+=val;
+		idx+=idx&-idx;
+	}
+}
+
+int pref(int idx)
+{
+	int ans=0;
+	while(idx>0)
+	{
+		ans+=bit[idx];
+		idx-=idx&-idx;
+	}
+
+	return ans;
+}
+
+int query(int l, int r)
+{
+    if(l>r)
+        return 0;
+
+	return pref(r) - pref(l-1);
+}

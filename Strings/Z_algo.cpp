06e468c16db9a5c0bb908062aa7fe9e3c728a7b2
hemant2132 <45938556+hemant2132@users.noreply.github.com>
2020-03-21 00:20:18 +0530

    Add files via upload

---
+++ Strings/Z_algo.cpp
@@ -0,0 +1,34 @@
+/*
+
+ z[i] -> longest substring starting from i which is also
+         a prefix of str
+ eg - for str="ababa", z={X,0,3,0,1}
+
+*/
+
+void z_algo(string str,int z[])
+{
+    int n=str.length();
+    int l=0,r=0;
+    for(int i=1;i<n;++i)
+    {
+        if(i>r)
+        {
+            for(l=r=i;r<n && str[r-l]==str[r];++r);
+            z[i]=r-l;
+            --r;
+        }
+        else
+        {
+            int k=i-l;
+            if(z[k]<r-i+1)
+                z[i]=z[k];
+            else
+            {
+                for(l=i;r<n && str[r-l]==str[r];++r);
+                z[i]=r-l;
+                --r;
+            }
+        }
+    }
+}
---
+++ Strings/manacher.cpp
@@ -0,0 +1,37 @@
+/*
+
+ p[0][i] -> no. of "even" length pals. centered at i (precisely, the one to the right of the exact center)
+ p[1][i] -> no. of "odd" length pals. centered at i  (except for pals. of length=1)
+
+ no. of pals. for i => radius for that i
+
+ eg - for str="abbbba"
+ p[0][i] = {0,0,1,3,1,0}
+ p[1][i] = {0,0,1,1,0,0}
+
+*/
+
+void manacher(string str)
+{
+	int n=str.length();
+
+	vector<vector<int>> p(2,vector<int>(n,0));
+
+	for(int z=0;z<2;++z)
+	{
+	    for(int i=0,l=0,r=0;i<n;++i)
+        {
+            int t=r-i+!z;
+
+            if(i<r)
+                p[z][i]=min(t,p[z][l+t]);
+
+            int L=i-p[z][i], R=i+p[z][i]-!z;
+            while(L-1>=0 && R+1<n && str[L-1]==str[R+1])
+                p[z][i]++, L--, R++;
+
+            if(R>r)
+                l=L, r=R;
+        }
+	}
+}

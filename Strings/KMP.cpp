d49e3f5e7888f551efdc7187380d6346e1029b94
hemant2132 <45938556+hemant2132@users.noreply.github.com>
2020-03-21 00:19:33 +0530

    Create KMP.cpp

---
+++ Strings/KMP.cpp
@@ -0,0 +1,53 @@
+/*
+
+ lps[i] -> longest proper prefix of pat[0..i] which is also a suffix of pat[0..i]
+ eg - for pat="abab", lps[] = {0,0,1,2}
+
+*/
+
+void getLPS(string pat,int lps[])
+{
+    int m=pat.length();
+
+    lps[0]=0;
+    for(int i=1,j;i<m;++i)
+    {
+        for(j=lps[i-1];j>0 && pat[i]!=pat[j];j=lps[j-1]);
+
+        if(pat[i]==pat[j])
+            ++j;
+
+        lps[i]=j;
+    }
+}
+
+int getOccurrences(string str,string pat,int lps[])
+{
+    int n=str.length();
+    int m=pat.length();
+
+    int ans=0;
+    for(int i=0,j=0;i<n;)
+    {
+        if(pat[j]==str[i])
+        {
+            ++i;
+            ++j;
+        }
+
+        if(j==m)
+        {
+            ++ans;
+            j=lps[j-1];
+        }
+        else if(i<n && pat[j]!=str[i])
+        {
+            if(j)
+                j=lps[j-1];
+            else
+                ++i;
+        }
+    }
+
+    return ans;
+}

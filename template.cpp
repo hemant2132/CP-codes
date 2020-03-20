94418aa52ac3d34c934b4321d8a4a143d31e5a25
hemant2132 <45938556+hemant2132@users.noreply.github.com>
2020-03-21 00:38:20 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,50 @@
+/*!enjoy karo yaar!*/
+
+//CF,CC,AtC,SPOJ: hp1999
+//HE: hemant269
+//HR: hemant2132
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define     ll           long long int
+#define     int          ll
+#define     ld           long double
+#define     fast()       ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define     all(x)       x.begin(),x.end()
+#define     mem(a,b)     memset(a,b,sizeof(a))
+#define     gcd(a,b)     (__gcd((a),(b)))
+#define     lcm(a,b)     (((a)*(b))/gcd((a),(b)))
+#define     pb           push_back
+#define     ins          insert
+#define     pii          pair<int,int>
+#define     F            first
+#define     S            second
+#define     makep        make_pair
+#define     maket        make_tuple
+#define     show(x)      for(auto c:x)cout<<c<<" ";cout<<"\n";
+#define     show2(x)     for(auto c:x)cout<<c.F<<" "<<c.S<<"\n";
+
+const ld pi=acos(-1);
+const int inf=1e18,M=1e9+7;
+const int N=1;
+
+void solve()
+{
+}
+
+int32_t main()
+{
+    fast();
+
+    int t=1;
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+        //cout<<"\n";
+    }
+
+    return 0;
+}
+

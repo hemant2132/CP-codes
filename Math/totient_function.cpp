58d2f29fc720d39ad8647914ad0ce0ba4c053c9c
hemant2132 <45938556+hemant2132@users.noreply.github.com>
2020-04-03 14:52:55 +0530

    Update totient_function.cpp

--- Math/totient_function.cpp
+++ Math/totient_function.cpp
@@ -21,19 +21,22 @@ int totient(int n)
     return res;
 }
 
-vector<int> phi(n);
+vector<int> phi(N);
 
-int totient_all()
+void totient_all()
 {
-    for(int i=0;i<n;++i)
+    for(int i=1;i<N;++i)
         phi[i]=i;
 
-    for(int i=2;i*i<n;++i)
+    for(int i=2;i<N;++i)
     {
         if(phi[i]==i)
         {
-            for(int j=i;j<n;j+=i)
-                phi[j]=(phi[j]/i)*(i-1);
+            for(int j=i;j<N;j+=i)
+            {
+                phi[j]/=i;
+                phi[j]*=(i-1);
+            }
         }
     }
 }

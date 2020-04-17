98353a160945c765c848e53931e4279aee6b8839
hemant2132 <45938556+hemant2132@users.noreply.github.com>
2020-04-17 16:19:08 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -7,26 +7,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define     ll           long long int
-#define     int          ll
-#define     ld           long double
-#define     fast()       ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define     all(x)       x.begin(),x.end()
-#define     mem(a,b)     memset(a,b,sizeof(a))
-#define     gcd(a,b)     (__gcd((a),(b)))
-#define     lcm(a,b)     (((a)*(b))/gcd((a),(b)))
-#define     pb           push_back
-#define     ins          insert
-#define     pii          pair<int,int>
-#define     F            first
-#define     S            second
-#define     makep        make_pair
-#define     maket        make_tuple
-#define     show(x)      for(auto c:x)cout<<c<<" ";cout<<"\n";
-#define     show2(x)     for(auto c:x)cout<<c.F<<" "<<c.S<<"\n";
+#define     int             long long int
+#define     fast()          ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define     all(x)          (x).begin(),(x).end()
+#define     rz(x)           resize(x)
+#define     mem(a,b)        memset(a,b,sizeof(a))
+#define     sz(x)           ((int)(x.size()))
+#define     pb              push_back
+#define     pf              push_front
+#define     pob             pop_back
+#define     pof             pop_front
+#define     ins             insert
+#define     vi              vector<int>
+#define     pii             pair<int,int>
+#define     mii             map<int,int>
+#define     F               first
+#define     S               second
+#define     makep           make_pair
+#define     maket           make_tuple
+#define     remax(a,b)      a=max(a,b)
+#define     remin(a,b)      a=min(a,b)
+#define     bitcount(x)     __builtin_popcountll(x)
+#define     iceil(n,x)      (((n)+(x)-1)/(x))
+#define     gcd(a,b)        (__gcd((a),(b)))
+#define     lcm(a,b)        (((a)*(b))/gcd((a),(b)))
+#define     dbug(x)         cout<<#x<<": "<<(x)<<"\n"
+#define     flush           fflush(stdout)
+#define     show(x)         for(auto zz:x)cout<<zz<<" ";cout<<"\n";
+#define     show2(x)        for(auto zz:x)cout<<zz.F<<" "<<zz.S<<"\n";
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
 
 const ld pi=acos(-1);
-const int inf=1e18,M=1e9+7;
+const ll inf=LLONG_MAX,M=1e9+7;
 const int N=1;
 
 void solve()
@@ -39,7 +54,7 @@ int32_t main()
 
     int t=1;
     //cin>>t;
-    while(t--)
+    for(int z=1;z<=t;++z)
     {
         solve();
         //cout<<"\n";
@@ -47,4 +62,3 @@ int32_t main()
 
     return 0;
 }
-

f930d0406e30c03109714defc4f96b90b11d2b0b
Hemant Panwar <45938556+hemant2132@users.noreply.github.com>
2020-06-24 12:49:05 +0530

    Debugging code added

--- template.cpp
+++ template.cpp
@@ -7,6 +7,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*-----<Defines>-----*/
 #define     int             long long int
 #define     fast()          ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define     all(x)          begin(x),end(x)
@@ -29,18 +30,67 @@ using namespace std;
 #define     remin(a,b)      a=min(a,b)
 #define     bitcount(x)     __builtin_popcountll(x)
 #define     iceil(n,x)      (((n)-1)/(x)+1)
-#define     dbug(x)         cout<<#x<<": "<<(x)<<"\n"
 #define     flush           fflush(stdout)
-#define     show(x)         for(auto zz:x)cout<<zz<<" ";cout<<"\n";
-#define     show2(x)        for(auto zz:x)cout<<zz.F<<" "<<zz.S<<"\n";
 
-typedef unsigned long long ull;
-typedef long long ll;
-typedef long double ld;
+using ull=unsigned long long;
+using ll=long long;
+using ld=long double;
+/*-----</Defines>-----*/
 
-const ld pi=acos(-1);
-const ll inf=1e18,M=1e9+7;
+/*-----<Debugging Stuff>-----*/
+string to_string(char s)
+{   string res="'''";res[1]=s;return res; }
+
+string to_string(string s)
+{   return '"'+s+'"'; }
+
+string to_string(const char* s)
+{   return to_string((string)s); }
+
+string to_string(bool b)
+{   return (b?"true":"false"); }
+
+template<typename A, typename B>
+string to_string(pair<A, B> p)
+{   return "("+to_string(p.F)+", "+to_string(p.S)+")"; }
+
+template<typename A>
+string to_string(A v)
+{
+    bool first=1; string res="{";
+    for(const auto &x:v)
+    {
+        if(!first)  res+=", ";
+        first=0;
+        res+=to_string(x);
+    }
+    res+="}";
+    return res;
+}
+
+void debug() { cout<<"\n"; }
+template<typename Head, typename... Tail>
+void debug(Head H, Tail... T)
+{
+    cout<<" "<<to_string(H);
+    debug(T...);
+}
+
+#define show(...) cout << "[" << #__VA_ARGS__ << "]:", debug(__VA_ARGS__)
+/*-----</Debugging Stuff>-----*/
+
+/*-----<Mod Operations>-----*/
+const int M=1e9+7;
+inline int add(int a,int b,int p=M){ a=a+b; if(a>=p) a-=p; return a; }
+inline int sub(int a,int b,int p=M){ a=a-b; if(a<0) a+=p; return a; }
+inline int mul(int a,int b,int p=M){ a=(a*1ll*b)%p; return a; }
+/*-----</Mod Operations>-----*/
+
+/*-----<Constants>-----*/
+const ld pi=acos(-1),eps=1e-9;
+const ll inf=1e18;
 const int N=1;
+/*-----</Constants>-----*/
 
 void solve()
 {

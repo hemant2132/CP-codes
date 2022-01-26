401b208e65025db0d309e8225b67e9150c25eaa5
Hemant Panwar <45938556+hemant2132@users.noreply.github.com>
2022-01-26 19:58:44 +0530

    Refactor: Strings

--- Strings/suffix_array_n(logn)2.cpp
+++ Strings/suffix_array_n(logn)2.cpp
@@ -1,83 +1,82 @@
 string txt;
 int n;
 
-vector<int> sa,tmp,pos;
+vector<int> sa, tmp, pos;
 vector<int> lcp;
-int gap,prv;
+int gap, prv;
 
-bool cmp(const int &x,const int &y)
-{
-    if(gap==1)
-    {
-        return txt[x]<txt[y];
+bool cmp(const int &x, const int &y) {
+    if (gap == 1) return txt[x] < txt[y];
+
+    int a;
+    if (x + prv >= n) {
+        a = -1;
+    } else {
+        a = pos[x + prv];
+    }
+    
+    int b;
+    if (y + prv >= n) {
+        b = -1;
+    } else {
+        b = pos[y + prv];
     }
 
-    int a,b;
-    if(x+prv>=n)
-        a=-1;
-    else
-        a=pos[x+prv];
-    if(y+prv>=n)
-        b=-1;
-    else
-        b=pos[y+prv];
-
-    return makep(pos[x],a)<makep(pos[y],b);
+    return makep(pos[x], a) < makep(pos[y], b);
 }
 
-void buildSA()
-{
-    sa.rz(n);pos.rz(n);tmp.rz(n);
+void buildSA() {
+    sa.rz(n);
+    pos.rz(n);
+    tmp.rz(n);
 
-    for(int i=0;i<n;++i)
-        sa[i]=i;
+    for (int i = 0; i < n; ++i)
+        sa[i] = i;
 
-    for(gap=1;;gap*=2)
-    {
-        sort(all(sa),cmp);
+    for (gap = 1; ; gap *= 2) {
+        sort(all(sa), cmp);
 
-        tmp[0]=0;
-        for(int i=1;i<n;++i)
-            tmp[i]=tmp[i-1]+cmp(sa[i-1],sa[i]);
+        tmp[0] = 0;
+        for (int i = 1; i < n; ++i)
+            tmp[i] = tmp[i - 1] + cmp(sa[i - 1], sa[i]);
 
-        for(int i=0;i<n;++i)
-            pos[sa[i]]=tmp[i];
+        for (int i = 0; i < n; ++i)
+            pos[sa[i]] = tmp[i];
 
-        if(*max_element(all(tmp))==n-1)
+        if (*max_element(all(tmp)) == n - 1)
             break;
 
-        prv=gap;
+        prv = gap;
     }
 }
 
-void buildLCP()
-{
-    int k=0;
-    lcp.rz(n-1);
-    for(int i=0;i<n;++i)
-    {
-        if(pos[i]==n-1)
-        {
-            k=0;
+void buildLCP() {
+    int k = 0;
+    lcp.rz(n - 1);
+    for (int i = 0; i < n; ++i) {
+        if (pos[i] == n - 1) {
+            k = 0;
             continue;
         }
 
-        int j=sa[pos[i]+1];
-        while(i+k<n && j+k<n && txt[i+k]==txt[j+k])
+        int j = sa[pos[i] + 1];
+        while (i + k < n && j + k < n && txt[i + k] == txt[j + k])
             k++;
-        lcp[pos[i]]=k;
+        lcp[pos[i]] = k;
 
-        if(k)
+        if (k)
             --k;
     }
 }
 
-void dispSA()
-{
-    cout<<"sa: ";show(sa);
-    cout<<"pos: ";show(pos);
-    cout<<"lcp: ";show(lcp);
+void dispSA() {
+    cout << "sa: ";
+    show(sa);
+    cout << "pos: ";
+    show(pos);
+    cout << "lcp: ";
+    show(lcp);
 
-    for(int i=0;i<n;++i)
-        cout<<txt.substr(sa[i])<<"\n";
+    for (int i = 0; i < n; ++i)
+        cout << txt.substr(sa[i]) << "\n";
 }
--- Strings/suffix_array_nlogn.cpp
+++ Strings/suffix_array_nlogn.cpp
@@ -1,124 +1,113 @@
 string txt;
 int n;
 
-vector<int> pos,sa;
-vector<int> cnt,nxt;
-vector<bool> bh,b2h;
+vector<int> pos, sa;
+vector<int> cnt, nxt;
+vector<bool> bh, b2h;
 
 vector<int> lcp;
 
-void init()
-{
-    sa.rz(n);pos.rz(n);
-    cnt.rz(n);nxt.rz(n);
-    bh.rz(n);b2h.rz(n);
-    lcp.rz(n-1);
+void init() {
+    sa.rz(n);
+    pos.rz(n);
+    cnt.rz(n);
+    nxt.rz(n);
+    bh.rz(n);
+    b2h.rz(n);
+    lcp.rz(n - 1);
 }
 
-bool smaller_first_char(int a,int b)
-{
-    return txt[a]<txt[b];
+bool smaller_first_char(int a, int b) {
+    return txt[a] < txt[b];
 }
 
-void buildSA()
-{
-    for(int i=0;i<n;++i)
-        sa[i]=i;
+void buildSA() {
+    for (int i = 0; i < n; ++i)
+        sa[i] = i;
 
-    sort(sa.begin(),sa.begin()+n,smaller_first_char);
+    sort(sa.begin(), sa.begin() + n, smaller_first_char);
 
-    for(int i=0;i<n;++i)
-    {
-        bh[i]= i==0 || txt[sa[i]]!=txt[sa[i-1]];
-        b2h[i]=false;
+    for (int i = 0; i < n; ++i) {
+        bh[i] = i == 0 || txt[sa[i]] != txt[sa[i - 1]];
+        b2h[i] = false;
     }
 
-    for(int h=1;h<n;h<<=1)
-    {
-        int buckets=0;
-        for(int i=0,j;i<n;i=j)
-        {
-            j=i+1;
-            while(j<n && !bh[j])
+    for (int h = 1; h < n; h <<= 1) {
+        int buckets = 0;
+        for (int i = 0, j; i < n; i = j) {
+            j = i + 1;
+            while (j < n && !bh[j])
                 ++j;
-            nxt[i]=j;
+            nxt[i] = j;
             ++buckets;
         }
 
-        if(buckets==n)
+        if (buckets == n)
             break;
 
-        for(int i=0;i<n;i=nxt[i])
-        {
-            cnt[i]=0;
-            for(int j=i;j<nxt[i];++j)
-                pos[sa[j]]=i;
+        for (int i = 0; i < n; i = nxt[i]) {
+            cnt[i] = 0;
+            for (int j = i; j < nxt[i]; ++j)
+                pos[sa[j]] = i;
         }
 
-        cnt[pos[n-h]]++;
-        b2h[pos[n-h]]=true;
-        for(int i=0;i<n;i=nxt[i])
-        {
-            for(int j=i;j<nxt[i];++j)
-            {
-                int s=sa[j]-h;
-                if(s>=0)
-                {
-                    int head=pos[s];
-                    pos[s]=head+cnt[head]++;
-                    b2h[pos[s]]=true;
+        cnt[pos[n - h]]++;
+        b2h[pos[n - h]] = true;
+        for (int i = 0; i < n; i = nxt[i]) {
+            for (int j = i; j < nxt[i]; ++j) {
+                int s = sa[j] - h;
+                if (s >= 0) {
+                    int head = pos[s];
+                    pos[s] = head + cnt[head]++;
+                    b2h[pos[s]] = true;
                 }
             }
 
-            for(int j=i;j<nxt[i];++j)
-            {
-                int s=sa[j]-h;
-                if(s>=0 && b2h[pos[s]])
-                {
-                    for(int k=pos[s]+1;!bh[k] && b2h[k];++k)
-                    b2h[k]=false;
+            for (int j = i; j < nxt[i]; ++j) {
+                int s = sa[j] - h;
+                if (s >= 0 && b2h[pos[s]]) {
+                    for (int k = pos[s] + 1; !bh[k] && b2h[k]; ++k)
+                        b2h[k] = false;
                 }
             }
         }
 
-        for(int i=0;i<n;++i)
-        {
-            sa[pos[i]]=i;
-            bh[i]=bh[i]|b2h[i];
+        for (int i = 0; i < n; ++i) {
+            sa[pos[i]] = i;
+            bh[i] = bh[i] | b2h[i];
         }
     }
 
-    for(int i=0;i<n;++i)
-        pos[sa[i]]=i;
+    for (int i = 0; i < n; ++i)
+        pos[sa[i]] = i;
 }
 
-void buildLCP()
-{
-    int k=0;
-    for(int i=0;i<n;++i)
-    {
-        if(pos[i]==n-1)
-        {
-            k=0;
+void buildLCP() {
+    int k = 0;
+    for (int i = 0; i < n; ++i) {
+        if (pos[i] == n - 1) {
+            k = 0;
             continue;
         }
 
-        int j=sa[pos[i]+1];
-        while(i+k<n && j+k<n && txt[i+k]==txt[j+k])
+        int j = sa[pos[i] + 1];
+        while (i + k < n && j + k < n && txt[i + k] == txt[j + k])
             k++;
-        lcp[pos[i]]=k;
+        lcp[pos[i]] = k;
 
-        if(k)
+        if (k)
             --k;
     }
 }
 
-void dispSA()
-{
-    cout<<"sa: ";nshow(sa,n);
-    cout<<"pos: ";nshow(pos,n);
-    cout<<"lcp: ";nshow(lcp,n-1);
+void dispSA() {
+    cout << "sa: ";
+    nshow(sa, n);
+    cout << "pos: ";
+    nshow(pos, n);
+    cout << "lcp: ";
+    nshow(lcp, n - 1);
 
-    for(int i=0;i<n;++i)
-        cout<<txt.substr(sa[i])<<"\n";
+    for (int i = 0; i < n; ++i)
+        cout << txt.substr(sa[i]) << "\n";
 }
